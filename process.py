a8d9f056ba89364128d82cc46a9791bc60793283
jeanmico <costellojeanm@gmail.com>
2018-07-27 16:54:22 -0700

    adding indicator for icd columns

--- process.py
+++ process.py
@@ -13,12 +13,25 @@ for i, col in enumerate(head_line):
 
 # columns of interest
 cols = set()
+icd_cols = set()
 col_fname = columns.txt
 with open(os.path.join(fpath, col_fname), 'r') as f:
-    lines = f.readlines()
-    for line in lines:
-        cols.add(line.strip())
-
+    for line in f:
+        vals = line.strip().split('\t')
+        cols.add(vals[0])
+        if vals[1] =='yes'
+            icd_cols.add(vals[0])
 print(cols)
+print(icd_cols)
 
+# indices of interest
+indices = []
+for col in cols:
+    indices.append(head[col])
+indices.sort()
 
+score_fpath = os.path.join(os.path.sep, 'wittelab', 'data1', 'chenan', 'ukbiobank', 'ad_mr', 'scores', 'score_files', 'SLE_gwascat_score')
+score_fname = 'SLE_gwascat_prs'
+
+with open(os.path.join(fpath, fname), 'r') as f:
+    
